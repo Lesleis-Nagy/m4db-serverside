55e0efd761dcca15db6b533b099ec9df5e72fd9e
Lesleis Nagy <lesleisnagy@gmail.com>
2021-11-01 13:30:03 +0000

    Field calculation tests account for return of raw data.
    The field calculation tests assumed that only a VTK unstructured grid was returned, now those test routines account for the fact that an unstructured grid *and* the raw data underlying the unstructured grid may be returned.

--- tests/postprocessing/test_field_calculations.py
+++ tests/postprocessing/test_field_calculations.py
@@ -9,16 +9,16 @@ from m4db_serverside.postprocessing.field_calculations import net_quantities
 class TestFieldCalculations(unittest.TestCase):
 
     def test_net_quantities(self):
-        ug = tec_to_unstructured_grid("../test_data/tecplot/magnetization.tec")
+        ug, _ = tec_to_unstructured_grid("../test_data/tecplot/magnetization.tec")
         quants = net_quantities(ug)
         print(quants)
 
     def test_write_vorticity_helicity_adm_to_tecplot(self):
-        ug = tec_to_unstructured_grid("../test_data/tecplot/magnetization.tec")
+        ug, _ = tec_to_unstructured_grid("../test_data/tecplot/magnetization.tec")
         write_vorticity_helicity_adm_to_tecplot(ug, "../test_data/tecplot/magnetization_plus_old.tec")
 
     def test_write_vorticity_helicity_adm_to_vtk(self):
-        ug = tec_to_unstructured_grid("../test_data/tecplot/magnetization.tec")
+        ug, _ = tec_to_unstructured_grid("../test_data/tecplot/magnetization.tec")
         write_vorticity_helicity_adm_to_vtk(ug, "../../test_data/tecplot/magnetization_plus.vtu")
 
 
