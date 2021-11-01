78e98cad18068a99fc30f9163fa7c7f3e13973d4
Lesleis Nagy <lesleisnagy@gmail.com>
2021-11-01 14:42:02 +0000

    Added logging, JSON model and 'data.zip' to create model procedure.

--- lib/m4db_serverside/runner/model/run_model.py
+++ lib/m4db_serverside/runner/model/run_model.py
@@ -7,9 +7,14 @@ import shutil
 import tempfile
 import random
 import time
+import zipfile
+
+import simplejson as json
+import numpy as np
 
 from subprocess import Popen, PIPE
 
+from m4db_database.utilities_logging import get_logger
 from m4db_database.configuration import read_config_from_environ
 from m4db_database.utilities import uid_to_dir
 
@@ -26,14 +31,23 @@ from m4db_serverside.postprocessing.field_calculations import net_quantities
 from m4db_serverside import global_vars
 
 
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
+
 def run_model(unique_id):
     config = read_config_from_environ()
+    logger = get_logger()
 
     # Wait a random amount of time based on unique_id.
     random.seed(unique_id)
     wait_time = random.randint(1, 20)
-    print("waiting for {}".format(wait_time))
+    print(f"waiting for {wait_time}s")
     time.sleep(wait_time)
+    print("activating ...")
 
     # This is the final destination of model data.
     database_dir = os.path.join(
@@ -41,11 +55,14 @@ def run_model(unique_id):
         global_vars.model_directory_name,
         uid_to_dir(unique_id)
     )
+    logger.debug(f"model destination: '{database_dir}")
 
     # This is the working directory.
     working_dir = config["m4db_serverside"]["working_dir"]
 
     with tempfile.TemporaryDirectory(dir=working_dir) as tmpdir:
+        logger.debug(f"working directory: '{tmpdir}'")
+
         # Switch to the working directory.
         os.chdir(tmpdir)
 
@@ -59,6 +76,8 @@ def run_model(unique_id):
         set_model_running_status(unique_id, "running")
 
         # Execute the merrill scripts.
+        logger.debug(f"executing {executable}")
+        merrill_t0 = time.time()
         cmd = "{exe:} {merrill_script:}".format(
                 exe=executable, merrill_script=global_vars.model_merrill_script_file_name
         )
@@ -71,9 +90,12 @@ def run_model(unique_id):
             shell=True
         )
         stdout, stderr = proc.communicate()
+        merrill_t1 = time.time()
+        time_taken = merrill_t1 - merrill_t0
+        logger.debug(f"merrill completed after {time_taken}s")
 
         # Check whether a file called "magnetization_mult.tec" was created - if so then rename it.
-        if (os.path.isfile(global_vars.magnetization_mult_tecplot_file_name)):
+        if os.path.isfile(global_vars.magnetization_mult_tecplot_file_name):
             os.rename(global_vars.magnetization_mult_tecplot_file_name, global_vars.magnetization_tecplot_file_name)
 
         # Write standard output and standard error to files.
@@ -91,8 +113,12 @@ def run_model(unique_id):
             return
 
         # Calculate additional quants.
-        ug = tec_to_unstructured_grid(global_vars.magnetization_tecplot_file_name)
+        ug, tec_raw = tec_to_unstructured_grid(global_vars.magnetization_tecplot_file_name)
         quants2 = net_quantities(ug)
+
+        # Save a JSON version of our model.
+        with open(global_vars.magnetization_json_file_name, "w") as fout:
+            fout.write(json.dumps(tec_raw, cls=NumpyEncoder))
 
         # Update quants.
         set_model_quants(unique_id,
@@ -116,12 +142,23 @@ def run_model(unique_id):
                          e_tot=quants1["tot_energy"],
                          volume=quants2["total_vol"])
 
+        # Compress each file in the directory.
+        logger.debug("Zipping files")
+        src_files = os.listdir(".")
+        src_zip_file = "data.zip"
+        zout = zipfile.ZipFile(src_zip_file, "w", zipfile.ZIP_DEFLATED)
+        for src_file in src_files:
+            logger.debug(f"{src_file} --> {src_zip_file}")
+            zout.write(src_file)
+        zout.close()
+
         # Copy all files to the final destination
         os.makedirs(database_dir, exist_ok=True)
-        src_files = os.listdir(".")
 
+        src_files = os.listdir(".")
         for file_name in src_files:
             if os.path.isfile(file_name):
+                logger.debug(f"Copying over file {file_name}")
                 shutil.copy(file_name, database_dir)
 
         # Set to finished.
